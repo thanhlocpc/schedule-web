eb8f478de5adb26819495ab23fd6d17eb8137d1f
thanhlocpc <nguyenthanhlocpc@gmail.com>
2023-02-03 00:08:31 +0700

    up

---
+++ src/auth-verify.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class AuthVerify extends Component {
+    constructor(props) {
+        props.history.listen(() => {
+            const tokenExpDate = localStorage.getItem("tokenExpDate");
+            if (tokenExpDate) {
+                if (parseInt(tokenExpDate) < Date.now()) {
+                    props.logout();
+                }
+            }
+        });
+    }
+    render() {
+        return <div></div>;
+    }
+}
+
+export default withRouter(AuthVerify);
