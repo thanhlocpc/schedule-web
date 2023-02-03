dedc86a9f0b559896434a6c89256a9569dfbd49b
thanhlocpc <nguyenthanhlocpc@gmail.com>
2023-02-03 08:25:38 +0700

    up

--- src/auth-verify.js
+++ src/auth-verify.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 
 class AuthVerify extends Component {
     constructor(props) {
+        super()
         props.history.listen(() => {
             const tokenExpDate = localStorage.getItem("tokenExpDate");
             if (tokenExpDate) {
